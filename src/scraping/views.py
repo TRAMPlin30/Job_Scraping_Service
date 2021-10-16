73220fa22b4da85a6d79ac40d690f0551ccaa8e1
TRAMPlin30 <vicodin28@gmail.com>
2021-10-16 09:48:31 +0300

    1 - sample (without logics)

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 def main_navbar(request):
     context = {'title': 'Job Finder', }
-    return render(request, "Navbar.html", context)
+    return render(request, "main_navbar.html", context)
 
 
